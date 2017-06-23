c36c50d093c58480fad15dbb95d1ee1963105f62
root <root@dev2.(none)>
2017-06-23 20:09:16 +0530

    script to find faulty disk and image processing app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jarvis
+# image processing
+1) Find faulty disks in the input image
+   - Find amber LEDs and mark these disk as FAIL
---
+++ image_processing/image_processor_app/src/main/java/com/cg/hackathon/imageprocessor/ImageProcessorMessage.java
@@ -0,0 +1,20 @@
+package com.cg.hackathon.imageprocessor;
+
+public class ImageProcessorMessage {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "ImageProcessorMessage [message=" + message + "]";
+	}
+
+}
