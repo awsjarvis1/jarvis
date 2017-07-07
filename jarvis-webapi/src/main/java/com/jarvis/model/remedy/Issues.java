e83ed744b614717e51fb788a4e25ac4bafbfe5b0
tnsnair <t-n-sooraj.nair@capgemini.com>
2017-07-07 15:29:19 +0530

    additions of new api

---
+++ jarvis-webapi/src/main/java/com/jarvis/model/remedy/Issues.java
@@ -0,0 +1,17 @@
+package com.jarvis.model.remedy;
+
+import java.util.List;
+
+public class Issues {
+
+	private List<ServiceRequest> serviceRequest = null;
+
+	public List<ServiceRequest> getServiceRequest() {
+		return serviceRequest;
+	}
+
+	public void setServiceRequest(List<ServiceRequest> serviceRequest) {
+		this.serviceRequest = serviceRequest;
+	}
+
+}
